f5d1ab4dbf2e082dfcb14725b91d5ff1cc503e62
JMurilloCortes <jealmuco@gmail.com>
2024-05-01 20:14:09 -0500

    Configurando rutas optimas

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Post, Put, Patch } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 
-@Controller({})
+
+@Controller("/tasks")
 
 export class TaskContoller{
 
   constructor(private tasksService: TasksService){}
 
-  @Get("/tasks")
+  @Get()
   getAllTasks(){
     return this.tasksService.getTasks();
+  }
+  @Post()
+  createTask(){
+    return this.tasksService.createTask();
+  }
+  @Put()
+  updateTask(){
+    return this.tasksService.updateTask();
+  }
+  @Delete()
+  deleteTask(){
+    return this.tasksService.deleteTask();
+  }
+  @Patch()
+  updateStatusTask(){
+    return this.tasksService.updateStatusTask();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,4 +6,17 @@ export class TasksService {
   getTasks() {
     return ["Task 1", "Task 2", "Task 3"]
   }
+  createTask() {
+    return "Creando tarea"
+  }
+  updateTask() {
+    return "Atualizando tarea"
+  }
+  deleteTask() {
+    return "Eliminando tarea"
+  }
+  updateStatusTask() {
+    return "Actualizando el estado de la tarea"
+  }
+ 
 }
